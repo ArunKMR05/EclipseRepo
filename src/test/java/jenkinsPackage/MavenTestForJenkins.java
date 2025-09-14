965557023eddb82f52196c33246ccd00c5188064
ArunKMR05 <119bcaa05arunkumarstcas@gmail.com>
2025-09-14 15:47:49 +0530

    Pushing all my Files from Eclipse to Git Hub

---
+++ src/test/java/jenkinsPackage/MavenTestForJenkins.java
@@ -0,0 +1,13 @@
+package jenkinsPackage;
+
+import org.testng.annotations.Test;
+
+public class MavenTestForJenkins {
+
+	@Test
+	public void testM() {
+		System.out.println("+++This is Maven Project for Running from the Jenkins+++");
+		
+	}
+
+}
